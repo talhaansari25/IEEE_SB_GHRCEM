1cf349f1e9b3f54ee739088015a735847874242c
Talha <talhaansari2026@gmail.com>
2025-04-08 02:17:29 +0530

    join

---
+++ src/components/JoinForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style.css";
+
+const JoinForm = () => {
+  return (
+    <div className="form-column">
+      <iframe
+        src="https://forms.gle/hthfcSvb95Ad4cWH9"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        allowFullScreen
+        title="Join Form"
+      ></iframe>
+    </div>
+  );
+};
+
+export default JoinForm;
