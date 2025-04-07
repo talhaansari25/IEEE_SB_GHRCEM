fef740cfb1efbb85e300897a3e841ac7ac893292
Talha <talhaansari2026@gmail.com>
2025-04-08 02:16:12 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-// src/App.jsx
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Events from './pages/Events';
-import Prize from './pages/Prize';
-import Contact from './pages/Contact';
-import Join from './pages/Join';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import Join from "./pages/Join.jsx";
+import Home from "./pages/home.jsx";
+import Events from "./pages/Events.jsx";
+import Prizes from "./pages/Prizes.jsx";
+
 
 function App() {
   return (
-    <>
-      {/* Removed <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/prize" element={<Prize />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/join" element={<Join />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/prizes" element={<Prizes/>}/>
+    </Routes>
   );
 }
 
